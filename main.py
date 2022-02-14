0f2a9297aca376c3a8bd24d8cf43a23c0b27224e
Sancara <santiagocaraballo@gmail.com>
2022-02-14 11:27:57 -0300

    flask flashes, successfully loged

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, \
+    render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -56,6 +57,8 @@ def hello():
         session['username'] = username
         password = login_form.password.data
         session['password'] = password
+
+        flash('Nombre de usuario registrado con éxito!')
 
         return redirect(url_for('index'))
     return render_template('hello.html', **context)
