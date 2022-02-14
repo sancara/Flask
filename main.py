ede6d19b22f54d8eaef5498a2c44b66f737743df
Sancara <santiagocaraballo@gmail.com>
2022-02-13 23:15:57 -0300

    Iterating over the template with a for loop

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Lau', 'San', 'Cata', 'Simi', '???']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -15,5 +17,14 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip=user_ip)
+    #crea un diccionario con todas las variables
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    #al pasar el **kwarg es como pasar las variables 1 por 1
+    #pudiendo acceder a ellas directamente,
+    #si pasaramos sólo context, deberíamos acceder a las variables
+    #como context.user_ip
+    return render_template('hello.html', **context)
 
