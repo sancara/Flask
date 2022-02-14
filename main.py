c28d80db982a824ff9e6f997a15f2da3b84ae313
Sancara <santiagocaraballo@gmail.com>
2022-02-14 10:04:24 -0300

    making the form post action

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 todos = ['Lau', 'San', 'Cata', 'Simi', '???']
+
+app.config['SECRET_KEY'] = 'ASDFñlkj!23'
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -19,17 +29,19 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     #crea un diccionario con todas las variables
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login': login_form
     }
     #al pasar el **kwarg es como pasar las variables 1 por 1
     #pudiendo acceder a ellas directamente,
